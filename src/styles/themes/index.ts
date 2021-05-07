3c0dd72d5a027ad1877a03e1adc9607f5b9f7956
Joonas Longi <joonaslongi@Joonas-MacBook-Pro.local>
2021-05-07 16:09:09 +0300

    add fonts, fix asset prefix

--- src/styles/themes/index.ts
+++ src/styles/themes/index.ts
@@ -13,6 +13,20 @@ const theme = createMuiTheme({
             main: red.A400,
         },
     },
+    typography: {
+        fontFamily: [
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(','),
+    },
 })
 
 export default theme
